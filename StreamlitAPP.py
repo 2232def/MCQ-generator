37eea31f1aa4d6c5e41e1bf86a3a697a1b846bb6
Dev Kumar Singh <gohi772004@gmail.com>
2025-05-22 05:23:29 +0530

    Added streamlit for userinterface

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -7,3 +7,14 @@ from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from src.mcqgenerator.logger import logging
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
+
+with open('D:\Users\Lenovo\Downloads\mcqgen\Response.json','r') as file:
+    RESPONSE_JSON = json.load(file)
+
+st.write("Streamlit version:", st.__version__)
+
+st.title("MCQ Generator Application with LangChain and Gemini")
+
+with st.form("user_inputs"):
+
+    uploaded_file = st.file_uploader("upload a PDF or txt file")
