28d8512cf859bebac8b35ae306f21be48dbb4c8b
Dev Kumar Singh <gohi772004@gmail.com>
2025-05-22 18:35:04 +0530

    added steamlit

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -7,8 +7,11 @@ from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from src.mcqgenerator.logger import logging
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
+from langchain.callbacks import StdOutCallbackHandler
 
-with open('D:\Users\Lenovo\Downloads\mcqgen\Response.json','r') as file:
+handler = StdOutCallbackHandler()
+
+with open(r'D:\Users\Lenovo\Downloads\mcqgen\Response.json','r') as file:
     RESPONSE_JSON = json.load(file)
 
 st.write("Streamlit version:", st.__version__)
@@ -18,3 +21,51 @@ st.title("MCQ Generator Application with LangChain and Gemini")
 with st.form("user_inputs"):
 
     uploaded_file = st.file_uploader("upload a PDF or txt file")
+
+    mcq_count = st.number_input("No. of MCQs", min_value=3, max_value=50)
+
+    subject = st.text_input("Insert Subject",  max_chars=20)
+
+    tone = st.text_input("Complexity Level of Questions", max_chars=20, placeholder="Simple")
+
+    button = st.form_submit_button("Generate MCQs") 
+
+    if button and uploaded_file is not None and mcq_count and subject and tone:
+        with st.spinner("Loading...."):
+            try:
+                text = read_file(uploaded_file)
+
+                with StdOutCallbackHandler() as cb:
+                    response = generate_evaluate_chain({
+                        "text":text,
+                        "number":mcq_count,
+                        "subject":subject,
+                        "tone":tone,
+                        "response_json":json.dumps(RESPONSE_JSON) 
+                    }
+                ) 
+                    
+            except Exception as e:
+                traceback.print_exception(type(e),e,e.__traceback__)
+                st.error("error")
+
+            else:
+                print(f"Total Tokens: {cb.total_tokens}")
+                print(f"Prompt Tokens: {cb.prompt_tokens}")
+                print(f"Completion Tokens: {cb.completion_tokens}")
+                if isinstance(response, dict):
+                    quiz = response.get("quiz" , None)
+                    if quiz is not None:
+                        table_data = get_table_data(quiz)
+                        if table_data is not None:
+                            df = pd.DataFrame(table_data)
+                            df.index = df.index+1
+                            st.table(df)
+                            st.text_area(label="Review", value=response["review"])
+                        else:
+                            st.error("Error in the table data")
+                    else:
+                        st.error("Quiz is none")
+                else:
+                    st.write(response)
+
