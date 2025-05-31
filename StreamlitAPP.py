1880db91503155c4334e0ac758e0567d1f8c821d
Dev Kumar Singh <gohi772004@gmail.com>
2025-05-31 22:07:50 +0530

    Removed the dotenv from streamlitAPP

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -1,14 +1,12 @@
 import json
 import traceback
 import pandas as pd
-from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from src.mcqgenerator.logger import logging
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
 from langchain.callbacks import StdOutCallbackHandler
 
-load_dotenv()
 
 handler = StdOutCallbackHandler()
 
